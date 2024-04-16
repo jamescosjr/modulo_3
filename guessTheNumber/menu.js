b7a32c6ff4f1248eb987e4576c3ddbff44b9d219
James Junior <jamescosjr@gmail.com>
2024-04-15 23:44:53 -0300

    Letting the menu cleaner.

--- guessTheNumber/menu.js
+++ guessTheNumber/menu.js
@@ -6,41 +6,42 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
-let attempts;
+const LEVELS = {
+    "1": 10,
+    "2": 8,
+    "3": 6,
+    "4": 4,
+    "5": 2,
+    "6": "quit"
+};
+
+const WELCOME_MESSAGE = `
+=============== Welcome to Guess the Number ===============
+1. Very Easy
+2. Easy
+3. Normal
+4. Hard
+5. Very Hard
+6. Quit
+`;
 
 function showMenu() {
-    console.log("\n=============== Welcome to Guess the Number ===============");
-    console.log("1. Very Easy");
-    console.log("2. Easy");
-    console.log("3. Normal");
-    console.log("4. Hard");
-    console.log("5. Very Hard");
-    console.log("6. Quit\n");
+    console.log(WELCOME_MESSAGE);
+    rl.question("Choose your level: ", handleLevelSelection);
+}
 
-    rl.question("Choose your level: ", (level) => {
-        if (level === '1') {
-            attempts = 10;
-            letsPlay(attempts, rl);
-        } else if (level === '2') {
-            attempts = 8;
-            letsPlay(attempts, rl);
-        } else if (level === '3') {
-            attempts = 6;
-            letsPlay(attempts, rl);
-        } else if (level === '4') {
-            attempts = 4;
-            letsPlay(attempts, rl);
-        } else if (level === '5') {
-            attempts = 2;
-            letsPlay(attempts, rl);
-        } else if (level === '6') {
+function handleLevelSelection(level) {
+    if (LEVELS[level]) {
+        if (level === '6') {
             console.log("Thanks for playing! Goodbye.");
             rl.close();
-        } else {
-            console.log("Invalid option. Please choose a valid option.");
-            showMenu();
+            return;
         }
-    });
+        letsPlay(LEVELS[level], rl);
+    } else {
+        console.log("Invalid option. Please choose a valid option.");
+        showMenu();
+    }
 }
 
 module.exports = { showMenu };
